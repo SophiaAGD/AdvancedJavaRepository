2d4f1dd6b489326ce6642f2e2bab53bed112cfb8
Sophie <sophiabelenkov@gmail.com>
2021-05-01 00:33:09 +0300

    changes for test branch

--- src/app/App.java
+++ src/app/App.java
@@ -2,6 +2,6 @@ package app;
 
 class App{
     public static void main(String[] args){
-        System.out.println("Hello from Sophie!");
+        System.out.println("Hello from Sonya!!");
     }
 }
